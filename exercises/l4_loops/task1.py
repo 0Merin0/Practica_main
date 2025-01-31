514ab8e0f531a47678d3dc67fffe632c85ca1b3b
mari <bernetmeri@gmail.com>
2025-01-31 14:59:49 +0200

    task1

--- exercises/l4_loops/task1.py
+++ exercises/l4_loops/task1.py
@@ -3,7 +3,12 @@
 # HINT:
 # Use while loop to count sum from 0 to n (included)
 def sum_to(n: int) -> int:
-    pass
+    number = 0
+    res = 0
+    while number < n:
+        number += 1
+        res += number
+    return res
 
 
 # Do not change the below's code
