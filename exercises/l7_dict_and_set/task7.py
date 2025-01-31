d311cd5d0fd031de9c2a5701b6b31c514dd4ac1b
mari <bernetmeri@gmail.com>
2025-01-31 15:20:14 +0200

    task7

--- exercises/l7_dict_and_set/task7.py
+++ exercises/l7_dict_and_set/task7.py
@@ -4,6 +4,8 @@ from typing import Any
 # Write a body of the function. It should merge to dictionaries d1 and d2.
 # NOTE: the function should create a new dictionary. Do not modify dictionaries
 # passed to the function as arguments
+
+
 def merge(d1: dict[Any, Any], d2: dict[Any, Any]) -> dict[Any, Any]:
     return d1 | d2
 
