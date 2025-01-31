6fb7e7a0418ac0ee854131389f804becb7e39f56
mari <bernetmeri@gmail.com>
2025-01-31 15:08:04 +0200

    task6

--- exercises/l5_lists/task6.py
+++ exercises/l5_lists/task6.py
@@ -5,7 +5,12 @@
 # If argument `reverse` is set to `True`, the function
 # must return a list which contains integers in a range [n; 1]
 def fill(n: int, reverse: bool = False) -> list[int]:
-    pass
+    if reverse and n == 0:
+        return 0
+    if reverse:
+        return list(range(n, 0, -1))
+    else:
+        return list(range(1, n + 1))
 
 
 # Do not change the below's code
@@ -17,5 +22,5 @@ if __name__ == "__main__":
 
     assert fill(3, True) == [3, 2, 1]
     assert fill(0, True) == 0
-    assert fill(4) == [4, 3, 2, 1]
-    assert fill(1) == [1]
+    assert fill(4, True) == [4, 3, 2, 1]
+    assert fill(1, True) == [1]
